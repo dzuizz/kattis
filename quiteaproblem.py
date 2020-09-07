e4e407563bee33808fb03a7113f8b5b54d037bb2
dzuizz <59071340+dzuizz@users.noreply.github.com>
2020-09-07 20:03:33 +0800

    solved quite a problem

---
+++ quiteaproblem.py
@@ -0,0 +1,9 @@
+while True:
+    try:
+        line = input().lower()
+        if "problem" in line:
+            print("yes")
+        else:
+            print("no")
+    except EOFError:
+        break
